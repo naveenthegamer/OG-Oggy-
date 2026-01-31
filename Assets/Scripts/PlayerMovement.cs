5265f6dd0ed3fb10a664816c16276aa986ad185a
Aggy-non <aggyviswasam@gmail.com>
2026-01-31 22:29:39 +0530

    PlayerMovement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,14 +1,20 @@
 ﻿using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Windows;
 
 public class PlayerMovement : MonoBehaviour
 {
     private Vector2 moveInput;
     private float HorizontalMovement;
 
+    public GameObject colorchange;
+    SpriteRenderer guyColor;
+
     [Header("Movement")]
     [SerializeField] Rigidbody2D rb;
+    private bool canMove;
     //[SerializeField] float speed;
+    [SerializeField] float test;
 
     [Header("Acceleration")]
     //[SerializeField] float MaxSpeed;
@@ -22,10 +28,41 @@ public class PlayerMovement : MonoBehaviour
     private float timeInSameDirection = 0f;
     private int lastDirection = 0; // -1 = left, 1 = right, 0 = none
     private float currentMaxSpeed;
-    private float currentSpeedX;
+    private float currentSpeedX=0;
     private float accelRate;
     int dir;
     float targetSpeedX;
+
+    [Header("Jump Settings")]
+    [SerializeField] float jumpForce;// 10f;
+    [SerializeField] float chargeForceIncrement;//5f; // extra force per charge level\
+    [SerializeField] float chargeInterval = 2f;
+    [SerializeField] int maxChargeLevel;// = 3;        // max time to reach full charge
+    public int chargeLevel = 0;
+    private bool canChargeJump;
+    Vector2 jumpDir;
+
+    private float chargeTimer = 0f;
+    
+    private bool isCharging = false;
+    private float totalForce;
+    private int newLevel;
+
+    [Header("Gravity")]
+    public float baseGravity;
+    public float maxFallSpeed;
+    public float fallSpeedMultiplier;
+
+    [Header("Ground Check")]
+    public Vector2 boxSize;
+    public float groundCheckSize;
+    public LayerMask groundLayer;
+    public bool grounded;
+
+    [Header("Air Control")]
+    [SerializeField] float airControlMultiplier;
+    private float moveSpeed;
+
 
 
 
@@ -37,20 +74,79 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        Debug.Log($"current x speed is {currentSpeedX}");
+        //Debug.Log($"current x speed is {currentSpeedX}");
+        Gravity();
+        isGrounded();
+        FlagChecks();
+        
+
     }
 
-    private void FixedUpdate()
+    private void FixedUpdate()  
     {
-        PhysicsMove(HorizontalMovement);
+      PhysicsMove(HorizontalMovement);
     }
 
+    void LateUpdate()
+    {
+        if (isCharging)
+        {
+            chargeTimer += Time.deltaTime;
+
+            // Update charge level based on whole intervals
+            newLevel = Mathf.FloorToInt(chargeTimer / chargeInterval);
+            chargeLevel = Mathf.Clamp(newLevel, 0, maxChargeLevel);
+        }
+    }
+
+    private void FlagChecks()
+    {
+        if (chargeLevel > 0)
+        {
+            currentSpeedX = 0;
+            canMove = false;
+            canChargeJump = true;
+
+        }
+        if (!isCharging)
+        {
+            chargeLevel = 0;
+        }
+        if (chargeLevel == 0)
+        {
+            canMove = true;
+            //canChargeJump = false;
+        }
+
+        if (chargeLevel == 0)
+        {
+            guyColor = colorchange.GetComponent<SpriteRenderer>();
+            guyColor.color = Color.white;
+        }
+        else if (chargeLevel==1)
+        {
+            guyColor = colorchange.GetComponent<SpriteRenderer>();
+            guyColor.color = Color.yellow;
+        }
+        else if (chargeLevel == 2)
+        {
+            guyColor = colorchange.GetComponent<SpriteRenderer>();
+            guyColor.color = Color.orange;
+        }
+        if (chargeLevel == 3)
+        {
+            guyColor = colorchange.GetComponent<SpriteRenderer>();
+            guyColor.color = Color.red;
+        }
+    }
     public void PhysicsMove(float horDire)
     {
         // Track direction (-1, 0, 1)
-            dir = horDire > 0 ? 1 : horDire < 0 ? -1 : 0;
+        //dir = horDire > 0 ? 1 : horDire < 0 ? -1 : 0;
+        dir = (int)Mathf.Sign(horDire);
 
-        if (dir != 0)
+
+        if (dir != 0 && grounded)
         {
             if (dir == lastDirection)
             {
@@ -74,21 +170,126 @@ public class PlayerMovement : MonoBehaviour
 
         lastDirection = dir;
 
+        moveSpeed = grounded ? currentMaxSpeed : currentMaxSpeed * airControlMultiplier;
+
         // Target speed
-        targetSpeedX = horDire * currentMaxSpeed;
+        targetSpeedX = horDire * moveSpeed;
 
         // Accel/decel
         accelRate = (Mathf.Abs(targetSpeedX) > 0.01f) ? acceleration : deceleration;
-        currentSpeedX = Mathf.MoveTowards(rb.linearVelocityX, targetSpeedX, accelRate * Time.fixedDeltaTime);
-        rb.linearVelocity = new Vector2(currentSpeedX, rb.linearVelocity.y);
+        currentSpeedX = Mathf.MoveTowards(currentSpeedX, targetSpeedX, accelRate * Time.fixedDeltaTime);
+        if (canMove)
+        {
+            rb.linearVelocityX = currentSpeedX;
+        }
+    }
+
+    public void Gravity()
+    {
+        if (rb.linearVelocityY < 0)
+        {
+            rb.gravityScale = baseGravity * fallSpeedMultiplier;
+            rb.linearVelocityY = Mathf.Max(rb.linearVelocityY, -maxFallSpeed);
+        }
+        else
+        {
+            rb.gravityScale = baseGravity;
+        }
+    }
+
+    public void isGrounded()
+    {
+        if (Physics2D.BoxCast(transform.position, boxSize, 0, -transform.up, groundCheckSize, groundLayer))
+        {
+            
+            grounded = true;
+        }
+
+        else
+        {
+            grounded = false;
+        }
+    }
+
+    public void ChargeJump()
+    {
+        if (chargeLevel == 0 && grounded)
+        {
+            // Normal jump
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            Debug.Log("Normal jump performed");
+        }
+        else if (chargeLevel > 0 &&
+                 (Vector2.Dot(moveInput.normalized, Vector2.right) > 0.9f ||
+                  Vector2.Dot(moveInput.normalized, Vector2.left) > 0.9f))
+        {
+            // Horizontal charged jump (left or right)
+            totalForce = jumpForce + (chargeLevel * chargeForceIncrement);
+
+            // Pick direction
+            Vector2 jumpDir = Vector2.zero;
+            if (Vector2.Dot(lhs: moveInput.normalized, Vector2.right) > 0.9f)
+            {
+                jumpDir = new Vector2(test, 1f);
+                Debug.Log(message: "right");
+            }
+            else if (Vector2.Dot(moveInput.normalized, Vector2.left) > 0.9f)
+            {
+                jumpDir = new Vector2(-test, 1f); // left
+                Debug.Log("right");
+            }
+
+            rb.AddForce(jumpDir * totalForce, ForceMode2D.Impulse);
+            Debug.Log($"Horizontal charged jump performed at level {chargeLevel}, force {totalForce}, direction {jumpDir}");
+        }
+        else if (chargeLevel > 0)
+        {
+            // Vertical charged jump
+            totalForce = jumpForce + (chargeLevel * chargeForceIncrement);
+            rb.AddForce(Vector2.up * totalForce, ForceMode2D.Impulse);
+            Debug.Log($"Charged jump performed at level {chargeLevel}, force {totalForce}");
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = grounded ? Color.green : Color.red;
+
+        // Draw the BoxCast area
+        Vector3 castOrigin = transform.position;
+        Vector3 castDirection = -transform.up * groundCheckSize;
+
+        // Draw the box at the cast origin
+        Gizmos.DrawWireCube(castOrigin + castDirection, boxSize);
+
     }
 
 
 
 
-//-----------------------------------------------------------------------------------------------------------------------------
+    //-----------------------------------------------------------------------------------------------------------------------------
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            // Start charging
+            isCharging = true;
+            chargeTimer = 0f;
+            chargeLevel = 0;
 
 
+        }
+        else if (context.canceled)
+        {
+            // Release jump
+            isCharging = false;
+
+            ChargeJump();
+
+        }
+
+    }
     public void GetHorizontalDirection(InputAction.CallbackContext context)
     {
         HorizontalMovement = context.ReadValue<Vector2>().x;        
@@ -99,24 +300,10 @@ public class PlayerMovement : MonoBehaviour
         moveInput = context.ReadValue<Vector2>();
     }
 
-    public void Jump(InputAction.CallbackContext context)
-    {
-        Debug.Log("jumpingtime");
-        //charge jump
-    }
-
-
-
-
-
-    public void aircontrol()
-    {
-
-    }
+    
     //coyote time
     //jump input buffering
     //current vector
-    //charge boots shouldnt change velocity they should apply force OMG!
     //rb.velocity to 0 when shooting in opposite direction of movement
     //crouch
 
